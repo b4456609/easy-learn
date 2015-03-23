bcf4b70069bdf10ac584cd3a83f270d02baff889
b4456609 <b4456609@gmail.com>
2015-03-24 02:00:02 +0800

    add slideshare button

--- src/js/pack_handler.js
+++ src/js/pack_handler.js
@@ -1,8 +1,33 @@
 $(document).on("pageinit", "#new_pack_edit", function() {
   var height = $(window).height * 0.8;
   $('textarea#edit').editable({
+    'buttons': ['bold', 'italic', 'slideshare', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'color', 'formatBlock', 'blockStyle', 'align', 'insertOrderedList', 'insertUnorderedList', 'outdent', 'indent', 'createLink', 'insertImage', 'insertVideo', 'undo', 'redo', 'html', 'insertHorizontalRule', 'table', 'uploadFile'],
     inlineMode: false,
-    toolbarFixed: false
+    toolbarFixed: false,
+    customButtons: {
+      // Alert button with Font Awesome icon.
+      slideshare: {
+        title: 'insert Slideshare',
+        icon: {
+          type: 'font',
+
+          // Font Awesome icon class fa fa-*.
+          value: 'fa fa-slideshare'
+        },
+        callback: function() {
+          $.get("http://www.slideshare.net/api/oembed/2?url=http://www.slideshare.net/haraldf/business-quotes-for-2011&format=json",
+            function(data) {
+              alert(JSON.stringify(data));
+              var http = 'http:' + data.slide_image_baseurl + '1' + data.slide_image_baseurl_suffix;
+              var img = "<img src=" + http + ">";
+              console.log(img);
+              $('textarea#edit').editable("insertHTML", img, true);
+            });
+            //<img style="-webkit-user-select: none; cursor: zoom-in;" src="http://image.slidesharecdn.com/110103quotes2010-12-110103073149-phpapp01/95/slide-1-1024.jpg" width="809" height="607">
+        },
+        refresh: function() {}
+      }
+    }
   });
 });
 

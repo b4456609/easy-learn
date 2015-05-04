3b85bd89a8032439d7e21e5ec1ad733cae9007aa
b4456609 <b4456609@gmail.com>
2015-05-04 23:11:34 +0800

    add exit dialogs

--- src/js/index.js
+++ src/js/index.js
@@ -14,13 +14,29 @@ document.addEventListener("deviceReady", deviceReady, false);
 //Resolve gapReady in reponse to deviceReady event
 function deviceReady() {
   gapReady.resolve();
+  
+  //disable back button
+  document.addEventListener("backbutton", onBackKeyDown, false);
+  function onBackKeyDown(e) {
+    e.preventDefault();
+    navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No"); 
+    // Prompt the user with the choice
+  }
+
+  function onConfirm(button) {
+    if (button == 2) {//If User selected No, then we just do nothing
+      return;
+    } else {
+      navigator.app.exitApp();// Otherwise we quit the app.
+    }
+  }
 }
 
 /**
  * Catch "mobileinit" event which is fired when a jQueryMobile is loaded.
  * Ensure that we respond to this event only once.
  */
-$(document).one("mobileinit", function() {
+$(document).one("mobileinit", function () {
   jqmReady.resolve();
 });
 
@@ -52,18 +68,18 @@ function myAppLogic() {
   if (localStorage.getItem('user') === null) {
     login();
   }
-  else{
+  else {
     $('#home_title').text(folderName);
     display_pack();
     display_folder();
     $('#user_name').text((JSON.parse(localStorage.user)).name);
   }
 
-    $('#logout').click(logout);
+  $('#logout').click(logout);
 
 }
 
-$(document).on("pageshow", "#home", function() {
+$(document).on("pageshow", "#home", function () {
 
 
 

56375a84cc47ef2c8e61c93bbb989a8bd86310d8
Bernie <b4456609@gmail.com>
2015-05-14 16:14:51 +0800

    add connection plugin

--- src/js/sync.js
+++ src/js/sync.js
@@ -438,3 +438,19 @@ $(document).on("click", ".show-page-loading-msg", function () {
   .on("click", ".hide-page-loading-msg", function () {
   $.mobile.loading("hide");
 });
+
+function checkConnection() {
+  var networkState = navigator.connection.type;
+
+  var states = {};
+  states[Connection.UNKNOWN] = 'Unknown connection';
+  states[Connection.ETHERNET] = 'Ethernet connection';
+  states[Connection.WIFI] = 'WiFi connection';
+  states[Connection.CELL_2G] = 'Cell 2G connection';
+  states[Connection.CELL_3G] = 'Cell 3G connection';
+  states[Connection.CELL_4G] = 'Cell 4G connection';
+  states[Connection.CELL] = 'Cell generic connection';
+  states[Connection.NONE] = 'No network connection';
+
+  alert('Connection type: ' + states[networkState]);
+}

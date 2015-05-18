cd3040534e5df0ba22a23bebcf567f3d96cb569d
b4456609 <b4456609@gmail.com>
2015-05-19 00:29:13 +0800

    finish login

--- src/js/login_handler.js
+++ src/js/login_handler.js
@@ -17,18 +17,18 @@ function login_check() {
   if ((account === 'loko') && (password === '123456789')) {
     user.id = 'loko';
     user.name = '洛林';
-    success(user);
+    successLogin(user);
   } else if ((account === 'b') && (password === 'b')) {
     user.id = 'b4456609';
     user.name = 'Bernie';
-    success(user);
+    successLogin(user);
   } else {
     $('#msg').text('帳號或密碼有錯誤!!');
   }
 
 }
 
-function success(user) {
+function successLogin(user) {
   $('#msg').text('登入成功');
   localStorage.setItem('user', JSON.stringify(user));
   var folder = [{
@@ -58,29 +58,48 @@ function logout() {
 
 
 function login() {
-    $.mobile.changePage("login.html", {
-      transition: "pop",
-      reverse: false,
-      changeHash: false
-    });
+  $.mobile.changePage("login.html", {
+    transition: "pop",
+    reverse: false,
+    changeHash: false
+  });
 
-//  facebookConnectPlugin.login(["public_profile"],
-//    fbLoginSuccess,
-//    function (error) { console.log(error); }
-//    );
+
 }
 
 function fbLoginSuccess(userData) {
   //alert("UserInfo: " + JSON.stringify(userData));
   console.log(userData);
 
+  var user = {
+    "id": "user" + new Date().getTime(),
+    "name": "Default",
+    "setting": {
+      wifi_sync: true,
+      mobile_network_sync: true,
+      last_sync_time: 1419519614000,
+      version: 0,
+      modified: true
+    }
+  };
+
   facebookConnectPlugin.api("/me", [],
     function (result) {
-      alert("Result: " + JSON.stringify(result));
       console.log(result);
+      user.id = result.id;
+      user.name = result.name;
+      successLogin(user);
     },
     function (error) {
-      alert(JSON.stringify(error));
+      alert('登入失敗:' + JSON.stringify(error));
+      console.log(error);
     });
+
 }
 
+function facebook_login() {
+  facebookConnectPlugin.login(["public_profile"],
+    fbLoginSuccess,
+    function (error) { console.log(error); }
+    );
+}

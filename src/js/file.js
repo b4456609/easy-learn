0a710a94900d33cc8c1441ed5b99e9254bc66608
b4456609 <b4456609@gmail.com>
2015-05-28 02:25:47 +0800

    alert file name

--- src/js/file.js
+++ src/js/file.js
@@ -126,6 +126,12 @@ function export_data() {
       var content = zip.generate({ type: "arraybuffer" });
 
       writer.onwriteend = function (evt) {
+        navigator.notification.alert(
+          '檔案位置: /easylearn/' + filename,  // message
+          null,         // callback
+          '成功輸出',            // title
+          '完成'                  // buttonName
+          );
         console.log("zip create success");
       };
 
@@ -260,36 +266,36 @@ function export_data() {
 }
 
 function import_data() {
-  
+
 }
 
 
 function formatDate(date, format) {
-    if (!date) return;
-    if (!format) format = "yyyy-MM-dd";
-    switch(typeof date) {
-        case "string":
-            date = new Date(date.replace(/-/, "/"));
-            break;
-        case "number":
-            date = new Date(date);
-            break;
-    } 
-    if (!date instanceof Date) return;
-    var dict = {
-        "yyyy": date.getFullYear(),
-        "M": date.getMonth() + 1,
-        "d": date.getDate(),
-        "H": date.getHours(),
-        "m": date.getMinutes(),
-        "s": date.getSeconds(),
-        "MM": ("" + (date.getMonth() + 101)).substr(1),
-        "dd": ("" + (date.getDate() + 100)).substr(1),
-        "HH": ("" + (date.getHours() + 100)).substr(1),
-        "mm": ("" + (date.getMinutes() + 100)).substr(1),
-        "ss": ("" + (date.getSeconds() + 100)).substr(1)
-    };    
-    return format.replace(/(yyyy|MM?|dd?|HH?|ss?|mm?)/g, function() {
-        return dict[arguments[0]];
-    });                
+  if (!date) return;
+  if (!format) format = "yyyy-MM-dd";
+  switch (typeof date) {
+    case "string":
+      date = new Date(date.replace(/-/, "/"));
+      break;
+    case "number":
+      date = new Date(date);
+      break;
+  }
+  if (!date instanceof Date) return;
+  var dict = {
+    "yyyy": date.getFullYear(),
+    "M": date.getMonth() + 1,
+    "d": date.getDate(),
+    "H": date.getHours(),
+    "m": date.getMinutes(),
+    "s": date.getSeconds(),
+    "MM": ("" + (date.getMonth() + 101)).substr(1),
+    "dd": ("" + (date.getDate() + 100)).substr(1),
+    "HH": ("" + (date.getHours() + 100)).substr(1),
+    "mm": ("" + (date.getMinutes() + 100)).substr(1),
+    "ss": ("" + (date.getSeconds() + 100)).substr(1)
+  };
+  return format.replace(/(yyyy|MM?|dd?|HH?|ss?|mm?)/g, function () {
+    return dict[arguments[0]];
+  });
 }

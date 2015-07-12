a772e044d3626850bd9cb0f9d7f683979fa69444
b4456609 <b4456609@gmail.com>
2015-07-12 11:13:29 +0800

    fix view pack index

--- src/js/pack_handler.js
+++ src/js/pack_handler.js
@@ -77,30 +77,32 @@ $(document).on('pageinit', "#view_pack", function() {
   var pack = new Pack();
   pack.getPack(viewPackId);
 
-  //if index is on private and old set to lastest verison
-  var viewPrivateId = pack.version[viewPackVersion.index].private_id;
-  if(viewPrivateId !== ''){
-    for(var j in pack.version){
-      if(pack.version[j].private_id == viewPrivateId){
-        if(pack.version[j].version > pack.version[viewPackVersion.index].version){
-          viewPackVersion.index = j;
-        }
-      }
-    }
-  }
 
   //set look's version's index, check if index exits
-  if(viewPackVersion.index >= pack.version.length || viewPackVersion.index < 0){
+  if (viewPackVersion.index >= pack.version.length || viewPackVersion.index < 0) {
     var version_time = 0;
     var version_index_temp = null;
-    for(var i in pack.version){
-      if(pack.version[i].create_time > version_time){
+    for (var i in pack.version) {
+      if (pack.version[i].create_time > version_time) {
         version_time = pack.version[i].create_time;
         version_index_temp = i;
       }
     }
     viewPackVersion.index = version_index_temp;
   }
+
+  //if index is on private and old set to lastest verison
+  var viewPrivateId = pack.version[viewPackVersion.index].private_id;
+  if (viewPrivateId !== '') {
+    for (var j in pack.version) {
+      if (pack.version[j].private_id == viewPrivateId) {
+        if (pack.version[j].version > pack.version[viewPackVersion.index].version) {
+          viewPackVersion.index = j;
+        }
+      }
+    }
+  }
+
 
   //set look version's id
   viewPackVersion.id = pack.version[viewPackVersion.index].id;
